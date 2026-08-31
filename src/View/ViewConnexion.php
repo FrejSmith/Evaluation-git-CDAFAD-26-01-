0243c5240601fa489e9a4ef4de786789a8a99e3d
FrejSmith <frejo910@gmail.com>
2026-08-31 11:36:07 +0200

    feat: update connexion and register views

---
+++ src/View/ViewConnexion.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./public/assets/styles/main.css">
+<script src="./public/assets/scripts/main.js" async></script>
+<title>Connexion</title>
+</head>
+<body>
+<h1>Se connecter</h1>
+<form action="" method="post">
+<label for="email">Veuillez saisir votre adresse email :</label>
+<input type="email" name="email">
+<label for="password">Veuillez saisir votre mot de passe :</label>
+<input type="password" name="password">
+<input type="submit" value="connexion">
+</form>
+</body>
+</html><!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./public/assets/styles/main.css">
+<script src="./public/assets/scripts/main.js" async></script>
+<title>Connexion</title>
+</head>
+<body>
+<h1>Se connecter</h1>
+<form action="" method="post">
+<label for="email">Veuillez saisir votre adresse email :</label>
+<input type="email" name="email">
+<label for="password">Veuillez saisir votre mot de passe :</label>
+<input type="password" name="password">
+<input type="submit" value="connexion">
+</form>
+</body>
+</html>
+
+  UW PICO 5.09            File: src/View/ViewConnexion.php            Modified  
+
+                                
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Pg   ^K Cut Text  ^C Cur Pos   
+^X Exit      ^J Justify   ^W Where is  ^V Next Pg   ^U UnCut Text^T To Spell  
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="./public/assets/styles/main.css">
+<script src="./public/assets/scripts/main.js" async></script>
+<title>Connexion</title>
+</head>
+<body>
+<h1>Se connecter</h1>
+<form action="" method="post">
+<label for="email">Veuillez saisir votre adresse email :</label>
+<input type="email" name="email">
+<label for="password">Veuillez saisir votre mot de passe :</label>
+<input type="password" name="password">
+<input type="submit" value="connexion">
+</form>
+</body>
+</html>
+
+
